a7cd693055f2ea3aeaa408dab22a8af65460a3c7
Haeparic <v567v202@gmail.com>
2022-12-15 18:02:33 +0900

    axios 추가 및 useMatch 추가

--- src/pages/SongList.js
+++ src/pages/SongList.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useMatch } from "react-router-dom";
 
 const SongList = (props) => {
+  // 경로의 패턴을 비교한다.
+  const pathMatch = useMatch("/songs/:id");
+  let pathMatchId;
+  if (pathMatch) {
+    pathMatchId = pathMatch.params.id ? parseInt(pathMatch.params.id, 10) : -1;
+  }
+  // pathMatch.params : 파라메터 {id: '7'}
+  // pathMatch.pathname : 요청경로 "/songs/7"
+  // pathMatch.pathnameBase : 주소창 "/songs/7"
+  // pathMatch.pattern : 패턴을 출력 {}
   const list = props.songs.map((item) => {
     return (
-      <li key={item.id} className="list-group-item">
-        <Link to={`/songs/${item.id}`} style={{textDecoration:"none"}}>
+      <li
+        key={item.id}
+        className={
+          item.id === pathMatchId
+            ? "list-group-item list-group-item-secondary"
+            : "list-group-item"
+        }
+      >
+        <Link to={`/songs/${item.id}`} style={{ textDecoration: "none" }}>
           {item.title} ({item.musician})
           <span className="float-end badge bg-secondary">
             <i className="fa fa-play"></i>
           </span>
-
         </Link>
       </li>
     );
@@ -19,7 +35,7 @@ const SongList = (props) => {
     <div className="card card-body">
       <h2>SongList</h2>
       <ul className="list-group">{list}</ul>
-      <Outlet/>
+      <Outlet />
     </div>
   );
 };
